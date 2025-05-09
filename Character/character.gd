053a6e4a0b4e2aeaea5340c1b44b7dafed06c4ed
Izar2030 <simler765@gmail.com>
2025-05-08 22:21:00 -0400

    Fly to the moon

--- Character/character.gd
+++ Character/character.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 const GRAVITY = 410.0
+const JUMP_POWER = -300
 const WALK_SPEED = 200
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
+
+	if Input.is_action_just_pressed("up"):
+		velocity.y = JUMP_POWER
 
 	if Input.is_action_pressed("left"):
 		velocity.x = -WALK_SPEED
@@ -12,6 +16,7 @@ func _physics_process(delta):
 		velocity.x =  WALK_SPEED
 	else:
 		velocity.x = 0
+
 
 	# "move_and_slide" already takes delta time into account.
 	move_and_slide()
